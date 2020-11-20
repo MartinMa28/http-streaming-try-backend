5b6bd311b352e760c24089c2cb4dda98e21bad2a
Yilin Ma <1135485226@qq.com>
2020-11-20 13:47:29 -0800

    working on http sse

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const api = require('./routes/api');
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.get('/', (req, res) => {
   res.status(200).send({
     message: 'testing',
   });
 });
+
+mongoose.connect('mongodb://mongo-1:27017', { useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   app.listen()
+// });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
---
+++ src/routes/api.js
@@ -0,0 +1,48 @@
+const Task = require('../models/task');
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/new', (req, res) => {
+  Task.create(
+    {
+      task: req.body.task,
+    },
+    (err, task) => {
+      if (err) {
+        console.log(`CREATE Error: ${err}`);
+        res.status(500).send('Failed to create a task.');
+      } else {
+        res.status(200).json(task);
+      }
+    }
+  );
+});
+
+router.delete('/:id', (req, res) => {
+  Task.findById(req.params.id, (err, task) => {
+    if (err) {
+      console.log(`DELETE Error: ${error}`);
+      res.status(500).send('Failed to delete a task.');
+    } else if (task) {
+      task.remove(() => {
+        res.status(200).json(task);
+      });
+    } else {
+      res.status(404).send('Not found');
+    }
+  });
+});
+
+router.get('/', (req, res) => {
+  try {
+    const tasks = await Task.find();
+
+    console.log(`Find ${tasks.length} tasks`);
+    res.status(200).json({
+      tasks: tasks
+    })
+  } catch(err) {
+    throw err;
+  }
+})
